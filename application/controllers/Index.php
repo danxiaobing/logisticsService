56da69057b0dd6ecd3860ff93690c6c98275d2fa
huguocai <huguocai@chinayie.com>
2017-07-31 14:59:17 +0800

    精简文件

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -5,7 +5,6 @@ use Hprose\Client;
 
 class IndexController extends Yaf_Controller_Abstract
 {
-
     private $code = '0000';
     private $msg = 'success';
     private $data = array();
@@ -27,58 +26,6 @@ class IndexController extends Yaf_Controller_Abstract
      */
     public function IndexAction()
     {
-
-
-	//$params['uid'] = '';
-	//	$params['type'] ='';
-	//	$params['categoryId'] = '';
-	//	$params['regularMin'] = '';
-		$params['regularMax'] = '';
-		$params['priceMin'] = '';
-		$params['priceMax'] = '';
-		$params['deliverydate'] = '';
-		$params['warehouse'] = '';
-		$params['delivery'] = '';
-		$params['curtype'] = 1;
-		$params['productname'] = '';
         echo "service Index OK ";
-//		$params=[];
-
-			// $G = new GoodsModel(Yaf_Registry:: get("db"));
-			// $this->data=$G->getSupplyDemand($params, $page, $row);
-
-		//print_r($this->data);
-
-//		if($this->code=='0000'){
-//			$G = new GoodsModel(Yaf_Registry:: get("db"));
-//			var_dump($G->getSupplyDemand($params, $page, $row));
-//		}else{
-//			echo 'fuck off';
-//		}
-
-
-
-//        $Web = new WebModel(Yaf_Registry::get("db"), Yaf_Registry:: get("mc"));
-//        echo $Web->isEmail('asdasd.@sad.com');
-//
-//        $W = new WmsModel(Yaf_Registry::get("wms_db"));
-//        $res = $W->getStorageList(array(), 10, 1);
-
-//        $res = $this->updateGoodsByIdFunc('69293');
-        //  $OC =     Client::create( 'order', false);
-        // $OC = new OrderController($this->getRequest(),$this->getResponse(),$this->getView());
-        //  $info = $OC->orderInfoFunc('37226');
-//        $array = array(
-//            'model' => 'storageapi',
-//            'action' => 'getStorageStockList',
-//            'company_id' => '5112',
-//            'goodsno' => 'C00004',
-//            'qualityno'=> '国标优等品',
-//        );
-//        $info = $this->cargoAndStockFunc( 'HYA0001',$array);
-//        var_dump($info);
     }
-
-
-
 }
--- application/plugins/Global.php
+++ application/plugins/Global.php
@@ -16,65 +16,41 @@ class GlobalPlugin extends Yaf_Plugin_Abstract
     public function dispatchLoopStartup(Yaf_Request_Abstract $request, Yaf_Response_Abstract $response)
     {
     	  
-    	 $params = ($request->getRequest('params',''));
+    	$params = ($request->getRequest('params',''));
     	 
-    	 
-    	
-    	 if($params !=''){
+    	if($params !=''){
     	    
-    	     $secret = new blowfish();
+    	    $secret = new blowfish();
  
-           $data = $secret->decrypt($params); 
-    	 
+            $data = $secret->decrypt($params); 
+
+            $arr = explode('/',$data);
+            $controller = $arr[1];
+            $action = $arr[2];
+
+            if($controller=='' || $action =='') die('403');
+
+            $request->setControllerName($controller);
+            $request->setActionName($action);
+
+
+            $var = array();
     	     
-    	     $arr = explode('/',$data);
-    	     $controller = $arr[1];
-    	     $action = $arr[2];
-    	     
-    	     if($controller=='' || $action =='')
-    	          die('403');
-    	     
-    	     $request->setControllerName($controller);
-    	     $request->setActionName($action);
-    	     
-    	      
-    	     $var = array();
-    	     
-    	     for($i = 3 ;$i < count($arr); $i++){
+    	    for($i = 3 ;$i < count($arr); $i++){
     	          $key = $arr[$i];
     	          $i++;
     	          $value = $arr[$i];
     	          
     	          $var[$key] = $value;     
-    	     }
-    	     $request->setParam($var);
-  
-    	    
+    	    }
+    	    $request->setParam($var);
     	 } else {
     	     die('403');
     	 }
     	 
-//         if ('view' === $request->getParam('out') || $request->isGet())
-//         {
-//             Yaf_Dispatcher::getInstance()->setView(new View(APPLICATION_PATH.'/application/views', array('root' => WEB_ROOT)));
-//             Yaf_Registry::set('view', true);
-//         }
-//         else //isXmlHttpRequest, isPost, isPut, isDelete, isCli
          {
              Yaf_Dispatcher::getInstance()->disableView();
              Yaf_Registry::set('view', false);
          }
 	}
-  
-
-	/**
-	 * 操作处理结束之后执行
-	 * @return void
-	 */
-/*
-    public function dispatchLoopShutdown(Yaf_Request_Abstract $request, Yaf_Response_Abstract $response)
-    {
-
-	}
-*/
 }
