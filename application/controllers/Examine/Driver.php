181677b76f00fa88ea062d82981d8a95aa5f1797
huguocai <huguocai@chinayie.com>
2017-08-01 13:36:26 +0800

    新增文件

---
+++ application/controllers/Examine/Driver.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author  Andy
+ * @date    2017-8-1
+ * @version $Id$
+ */
+
+class Examine_DriverController extends Rpc {
+
+    public function init() {
+        parent::init();
+        
+    }
+
+
+}
