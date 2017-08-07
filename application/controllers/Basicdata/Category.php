05d2013838d371be8aab38efe1a3e312756d5572
huguocai <huguocai@chinayie.com>
2017-08-07 09:41:45 +0800

    产品类别

---
+++ application/controllers/Basicdata/Category.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * author Andy
+ * date 2017-8-2
+ */
+class Basicdata_CategoryController extends Rpc {
+
+    public function init() {
+        parent::init();
+        
+    }
+
+
+    //列表信息
+    public function getCateInfoFunc($serach){
+    	$C = new Basicdata_CategoryModel(Yaf_Registry::get("db"));
+    	return $C->getCateInfo($serach);
+    }
+
+    //新增类别
+    public function addProductFunc($input){
+      $C = new Basicdata_CategoryModel(Yaf_Registry::get("db"));
+      return $C->addCate($input);      
+    }
+
+
+    
+    //获取数据 by id
+    public function getCateByIdFunc($id){
+      $C = new Basicdata_CategoryModel(Yaf_Registry::get("db"));
+      return $C->getInfoById($id);       
+    }
+
+    //更新数据
+    public function updateCateFunc($id,$input){
+      $C = new Basicdata_CategoryModel(Yaf_Registry::get("db"));
+      return $C->updateCate($id,$input);   
+    }
+
+    //删除数据
+    public function deleteCateFunc($id){
+      $C = new Basicdata_CategoryModel(Yaf_Registry::get("db"));
+      return $C->deleteCate($id);
+    }
+
+
+}
