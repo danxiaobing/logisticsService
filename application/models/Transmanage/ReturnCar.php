32c0b4be76a1f71805a414168220f76ffea22df1
daifangli <daifangli@chinayie.com>
2017-08-30 16:52:50 +0800

    修改

--- application/models/Transmanage/ReturnCar.php
+++ application/models/Transmanage/ReturnCar.php
@@ -98,7 +98,36 @@ class Transmanage_ReturnCarModel
                         `product_id`,
                         `price`,
                         `status`
-                        FROM `gl_return_car` WHERE `id` = {$id}  AND `is_del`= 0 ";
+                        FROM `gl_return_car` WHERE `id` = {$id}  AND `is_del`= 0";
+
+      /*  $sql = "SELECT gl_return_car.`id`,
+                        gl_return_car.`cid`,
+                        gl_return_car.`start_province_id`,
+                        gl_return_car.`start_city_id`,
+                        start_city.`city` as start_city,
+                        gl_return_car.`start_area_id`,
+                        start_area.`area` as start_area,
+                        gl_return_car.`end_province_id`,
+                        gl_return_car.`end_city_id`,
+                        end_city.`city` as end_city,
+                        gl_return_car.`end_area_id`,
+                        end_area.`area` as end_area,
+                        gl_return_car.`start_time`,
+                        gl_return_car.`end_time`,
+                        gl_return_car.`price_type`,
+                        gl_return_car.`min_load`,
+                        gl_return_car.`max_load`,
+                        gl_return_car.`category_id`,
+                        gl_return_car.`product_id`,
+                        gl_return_car.`price`,
+                        gl_return_car.`status`
+                        FROM `gl_return_car`
+                        LEFT JOIN conf_city  start_city ON gl_return_car.`start_city_id` = start_city.`cityid`
+                        LEFT JOIN conf_city  end_city  ON gl_return_car.`end_city_id` = end_city.`cityid`
+                        LEFT JOIN conf_area  start_area ON gl_return_car.`start_area_id` = start_area.`areaid`
+                        LEFT JOIN conf_area  end_area  ON gl_return_car.`end_area_id` = end_area.`areaid`
+                        WHERE gl_return_car.`id` = {$id}  AND gl_return_car.`is_del`= 0";*/
+      
         return $this->dbh->select_row($sql);
     }
     //添加信息
