7dcf4e65f2dc69685cc3b8c3c009d0b22ac47136
huguocai <huguocai@chinayie.com>
2017-09-18 10:23:11 +0800

    Fix

--- application/controllers/Transmanage/Inquirydel.php
+++ application/controllers/Transmanage/Inquirydel.php
@@ -18,26 +18,26 @@ class Transmanage_InquirydelController extends Rpc {
      */
    public function getInquiryListFunc($search)
     {
-        $L = new Transmanage_InquiryDelModel(Yaf_Registry::get("db"));
+        $L = new Transmanage_InquirydelModel(Yaf_Registry::get("db"));
         return $L->getInquiryList($search);
     }
 
     /*获取询价单基本信息*/
     public function getGoodsInfoFunc($id){
-        $L = new Transmanage_InquiryDelModel(Yaf_Registry::get("db"));
+        $L = new Transmanage_InquirydelModel(Yaf_Registry::get("db"));
         return $L->getGoodsInfo($id);        
     }
 
 
     /*获取当前询价单的价格状态信息*/
     public function getInquiryInfoFunc($id){
-        $L = new Transmanage_InquiryDelModel(Yaf_Registry::get("db"));
+        $L = new Transmanage_InquirydelModel(Yaf_Registry::get("db"));
         return $L->getInquiryInfo($id);         
     }
 
     /*生成询价单、询价日志*/
     public function addReceiptFunc($data=array(),$price,$goodsid){
-        $L = new Transmanage_InquiryDelModel(Yaf_Registry::get("db"));
+        $L = new Transmanage_InquirydelModel(Yaf_Registry::get("db"));
         return $L->addReceipt($data,$price,$goodsid);  
     }
 
@@ -46,13 +46,13 @@ class Transmanage_InquirydelController extends Rpc {
      * return bool
      */
     public function cancalInquiryFunc($params){
-        $L = new Transmanage_InquiryDelModel(Yaf_Registry::get("db"));
+        $L = new Transmanage_InquirydelModel(Yaf_Registry::get("db"));
         return $L->cancalInquiry($params);
     }
 
     /*同意交易*/
     public function agreeInquiryFunc($data=array(),$inquiryid){
-        $L = new Transmanage_InquiryDelModel(Yaf_Registry::get("db"));
+        $L = new Transmanage_InquirydelModel(Yaf_Registry::get("db"));
         return $L->agreeInquiry($data,$inquiryid);        
     }
 
