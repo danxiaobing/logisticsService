6d60bbbe501aeb6b4907b6336a7e8f994e9f6c26
daifangli <daifangli@chinayie.com>
2018-06-12 09:17:32 +0800

    司机-承运商 服务层分离

---
+++ application/controllers/App/Carrier/Index.php
@@ -0,0 +1,23 @@
+<?php
+
+
+class App_Carrier_IndexController extends Rpc
+{
+
+    /**
+     * IndexController::init()
+     *
+     * @return void
+     */
+    public function init() {
+        parent::init();
+
+    }
+
+   public function IndexFunc(){
+
+     echo 'ok';
+
+   }
+
+}
---
+++ application/controllers/App/Driver/Usercenter.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/5/17
+ * Time: 10:28
+ */
+class App_Driver_UsercenterController extends Rpc
+{
+
+    public function init()
+    {
+        parent::init();
+
+    }
+
+    /**
+     * 意见反馈
+     * @param array $params
+     * @return array $data
+     */
+    public function feedbackFunc($params){
+        $feedback = new App_Driver_UsercenterModel(Yaf_Registry::get("db"));
+        $data = $feedback->add($params);
+        return $data;
+    }
+
+    public function getVersionsFunc($type){
+        $Auth = new App_Driver_UsercenterModel(Yaf_Registry:: get("gy_db"));
+        return $Auth->getVersions($type);
+    }
+
+}
