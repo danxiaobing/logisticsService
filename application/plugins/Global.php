558e4211ba4f03ea8c620dd5573d73caa35b8338
daifangli <daifangli@chinayie.com>
2018-06-12 11:41:13 +0800

    恢复文件

--- application/plugins/Global.php
+++ application/plugins/Global.php
@@ -5,75 +5,52 @@
  * @date    2011-10-25 15:00
  * @version $Id$
  */
-class GlobalPlugin extends Yaf_Plugin_Abstract {
 
-    /**
-     * 操作正式处理之前执行，判断输出设定
-     * @return void
-     */
-    public function dispatchLoopStartup(Yaf_Request_Abstract $request, Yaf_Response_Abstract $response) {
-        $controller = strtolower($request->getControllerName());
-        if ($controller == 'wordpass') return;
-        if ($controller == 'web') return;
-        $params = ($request->getRequest('params', ''));
+class GlobalPlugin extends Yaf_Plugin_Abstract
+{
 
+	/**
+	 * 操作正式处理之前执行，判断输出设定
+	 * @return void
+	 */
+    public function dispatchLoopStartup(Yaf_Request_Abstract $request, Yaf_Response_Abstract $response)
+    {
 
-        //支持明文传输
-        if ($params == '') {
-            $req = $request->getRequest();
-            // $req+= $request->getParams();
-            $var = '';
-            foreach ($req as $name => $val) {
-                //echo $name.':'.$val."\n";
-                $var.= $name . "=" . $val . "&";
-            }
-            $secret = new blowfish();
-            $var = $secret->encrypt($var);
-            $params = $var;
-        }
+    	$params = ($request->getRequest('params',''));
 
+    	if($params !=''){
 
-        if ($params != '') {
-            $secret = new blowfish();
-            $string = $secret->decrypt($params);
-            // $string = 'model=index&action=gettime';
-            // $string = $secret->encrypt($string);
-            // echo $string;
-            parse_str($string, $data);
-            $modules = $data['modules'];
-            $controller = $data['model'];
-            $action = $data['action'];
-            if ($controller == '' || $action == '') {
-                $arr = array(
-                    'code' => '403'
-                );
-                die(json_encode($arr));
-            }
-            $controller = ucfirst($controller);
-            $modules = ucfirst($modules);
-            $request->setModuleName($modules);
+    	    $secret = new blowfish();
+
+            $data = $secret->decrypt($params);
+
+            $arr = explode('/',$data);
+            $controller = $arr[1];
+            $action = $arr[2];
+
+            if($controller=='' || $action =='') die('403');
+
             $request->setControllerName($controller);
             $request->setActionName($action);
-            unset($data['modules']);
-            unset($data['model']);
-            unset($data['action']);
-            $request->setParam($data);
-        } else {
-            $arr = array(
-                'code' => '403'
-            );
-            die(json_encode($arr));
-        }
-    }
-    /**
-     * 操作处理结束之后执行
-     * @return void
-     */
-    /*
-    public function dispatchLoopShutdown(Yaf_Request_Abstract $request, Yaf_Response_Abstract $response)
-    {
-    
-    }
-    */
-}
 
+
+            $var = array();
+
+    	    for($i = 3 ;$i < count($arr); $i++){
+    	          $key = $arr[$i];
+    	          $i++;
+    	          $value = $arr[$i];
+
+    	          $var[$key] = $value;
+    	    }
+    	    $request->setParam($var);
+    	 } else {
+    	     die('403');
+    	 }
+
+         {
+             Yaf_Dispatcher::getInstance()->disableView();
+             Yaf_Registry::set('view', false);
+         }
+	}
+}
